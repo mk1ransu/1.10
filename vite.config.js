c0cad5f55b90f9878cbaf65d1c5ddafef70fc851
mk1ransu <raikeruraansu@gmail.com>
2024-12-30 02:57:21 +0800

    Redeploy with new changes

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default {
-  optimizeDeps: {
-    include: ['three'],
-  },
-};
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['three', 'lil-gui'],  // Add external modules here
+    }
+  }
+});
